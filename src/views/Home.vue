0e75fe2944b306a5cbe4e341be5bbeef1f5b7931
Dane Petersen <thegreatsunra@gmail.com>
2018-05-12 13:49:54 -0500

    Show users and their respective things, with proper actions, on home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,80 @@
 <template>
   <div class="home">
-    <h1>Create User</h1>
+    <div v-for="user in users" :key="user.id">
+      <h1>{{user.firstName}} {{user.lastName}} ({{user.id}})</h1>
+      <h3>Things</h3>
     <form>
-      <input type="text" name="firstName" v-model="newUser.firstName" placeholder="firstName"><br>
-      <input type="text" name="lastName" v-model="newUser.lastName" placeholder="lastName"><br>
-      <label><input type="checkbox" name="isAdmin" v-model="newUser.isAdmin"> isAdmin</label><br>
-      <input type="submit" @click.prevent="createUser()" value="Create User">
+      <input type="text" placeholder="name" v-model.trim="user.newThing.name" >
+      <input type="submit" @click.prevent="createThing(user.id, user.newThing.name)" value="Create">
     </form>
 
-    <h1>Users</h1>
     <table>
-      <tr v-for="user in users">
+      <tr v-for="thing in things" :key="thing.id">
         <td>
-          <span v-if="editableUser !== user.id">{{user.firstName}} {{user.lastName}}</span><br>
-          <span v-if="editableUser === user.id">
-            <input type="text" name="firstName" v-model="user.firstName"> <input type="text" name="lastName" v-model="user.lastName">
+          {{thing.id}}
+        </td>
+        <td>
+          {{thing.userId}}
+        </td>
+        <td>
+          <span v-if="editableThing !== thing.id || thing.userId !== user.id">{{thing.name}}</span>
+          <span v-if="editableThing === thing.id && thing.userId === user.id">
+            <input type="text" name="name" v-model.trim="thing.name">
           </span>
         </td>
         <td>
-          <span v-if="user.isAdmin && editableUser !== user.id">isAdmin</span><br>
-          <label v-if="editableUser === user.id"><input type="checkbox" name="isAdmin" v-model="user.isAdmin"> isAdmin</label>
-        </td>
-        <td>
-          <button v-if="editableUser !== user.id" @click.prevent="editUser(user.id)">Edit</button>
-          <button v-if="editableUser === user.id" @click.prevent="editUser(user.id)">Save</button>
-          <button @click.prevent="destroyUser(user.id)">Delete</button>
+          <button v-if="editableThing !== thing.id && thing.userId === user.id" @click.prevent="editThing(thing.id)">Edit</button>
+          <button v-if="editableThing === thing.id && thing.userId === user.id" @click.prevent="editThing(thing.id)">Save</button>
+          <button @click.prevent="destroyThing(thing.id)" v-if="editableThing !== thing.id && thing.userId === user.id">Delete</button>
         </td>
       </tr>
     </table>
-
-    <!-- <img src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   data () {
     return {
       users: this.$store.state.Users,
-      newUser: {
-        firstName: '',
-        lastName: '',
-        isAdmin: false
-      },
-      editableUser: ''
+      things: this.$store.state.Things,
+      editableThing: ''
     }
   },
+  computed: {
+  },
   components: {
-    HelloWorld
   },
   methods: {
-    destroyUser (id) {
-      this.$store.dispatch('destroyUser', id)
+    destroyThing (id) {
+      this.$store.dispatch('destroyThing', id)
     },
-    editUser (id) {
-      if (!this.editableUser) {
-        this.editableUser = id
+    editThing (id) {
+      if (!this.editableThing) {
+        this.editableThing = id
       } else {
-        this.editableUser = ''
+        this.editableThing = ''
       }
     },
-    resetNewUser () {
-      this.newUser.firstName = ''
-      this.newUser.lastName = ''
-      this.newUser.isAdmin = false
+    resetNewThing (userId) {
+      const payload = {
+        id: userId,
+        newThing: {
+          name: ''
+        }
+      }
+      this.$store.dispatch('updateUser', payload)
     },
-    createUser () {
-      this.newUser.firstName = this.newUser.firstName.trim()
-      this.newUser.lastName = this.newUser.lastName.trim()
-      if (this.newUser.firstName && this.newUser.lastName) {
-        this.$store.dispatch('createUser', this.newUser)
-        this.resetNewUser()
+    createThing (userId, thingName) {
+        if (thingName !== '') {
+          const newThing = {
+            userId,
+            name: thingName
+          }
+          this.$store.dispatch('createThing', newThing)
+          this.resetNewThing(userId)
       }
     }
   }
