39a7d11932d507b8dfbaf694ae1af66e74b321e9
Dane Petersen <thegreatsunra@gmail.com>
2018-05-12 09:00:51 -0500

    Stub out user store

--- src/store/modules/Users.js
+++ src/store/modules/Users.js
@@ -3,9 +3,21 @@ import Vue from 'vue'
 const state = []
 
 const mutations = {
+  CREATE_USER (state, payload) {
+  },
+  UPDATE_USER (state, payload) {
+  },
+  DESTROY_USER (state, userId) {
+  }
 }
 
 const actions = {
+  createUser: async ({ commit }, payload) => {
+  },
+  updateUser: async ({ commit }, payload) => {
+  },
+  destroyUser: async ({ commit }, userId) => {
+  }
 }
 
 const getters = {
