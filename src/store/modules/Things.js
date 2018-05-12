2c95aecb8e5dfd43a950d74ee6974132c2b00b71
Dane Petersen <thegreatsunra@gmail.com>
2018-05-12 13:49:05 -0500

    Rough out actions and mutations for things

--- src/store/modules/Things.js
+++ src/store/modules/Things.js
@@ -1,11 +1,62 @@
 // import Vue from 'vue'
+import random from '@/random'
 
-const state = []
+/*
+{
+  id,
+  name,
+  userId
+}
+*/
+
+const state = [
+]
 
 const mutations = {
+  CREATE_THING (state, payload) {
+    state.push({
+      id: random.createString(8),
+      name: payload.name,
+      userId: payload.userId
+    })
+  },
+  UPDATE_THING (state, payload) {
+
+  },
+  DESTROY_THING (state, thingId) {
+    state.forEach(({ id }, index) => {
+      if (id === thingId) {
+        state.splice(index, 1)
+      }
+    })
+  }
 }
 
 const actions = {
+  createThing: async ({ commit }, payload) => {
+    try {
+      console.log('creating new thing')
+      await commit('CREATE_THING', payload)
+    } catch (err) {
+      console.log('error creating thing', err)
+    }
+  },
+  updateThing: async ({ commit }, payload) => {
+    try {
+      console.log('updating existing thing')
+      await commit('UPDATE_THING', payload)
+    } catch (err) {
+      console.log('error updating thing', err)
+    }
+  },
+  destroyThing: async ({ commit }, thingId) => {
+    try {
+      console.log('destroying thing')
+      await commit('DESTROY_THING', thingId)
+    } catch (err) {
+      console.log('error destroying thing', err)
+    }
+  }
 }
 
 const getters = {
