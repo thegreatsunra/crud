c84190877f2fa7436e7c0a7288f3d600eb81745d
Dane Petersen <thegreatsunra@gmail.com>
2018-05-14 08:12:50 -0500

    Apply browser chrome styles to admin view

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,36 +1,53 @@
 <template>
-  <div class="home">
-    <h1>Create User</h1>
-    <form>
-      <input type="text" name="firstName" v-model.trim="newUser.firstName" placeholder="firstName"><br>
-      <input type="text" name="lastName" v-model.trim="newUser.lastName" placeholder="lastName"><br>
-      <label><input type="checkbox" name="isAdmin" v-model.trim="newUser.isAdmin"> isAdmin</label><br>
-      <input type="submit" @click.prevent="createUser()" value="Create User">
-    </form>
+  <div class="_wrapper">
+    <div class="_browser">
+      <div class="_browser__chrome">
+        <div class="_browser__button _browser__button--danger"></div>
+        <div class="_browser__button _browser__button--warning"></div>
+        <div class="_browser__button _browser__button--success"></div>
+      </div>
+      <div class="_browser__content">
+        <div class="_app">
+          <div class="_app__header">
+            My Application
+          </div>
+          <div class="_app__nav">
+            <div class="_avatar" v-bind:title="`User ID: NONE`"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" width="16" height="16"><g class="nc-icon-wrapper" fill="#aaaaaa"><path d="M4.318,9A5,5,0,0,0,1.5,13.5h0s2,2,6.5,2,6.5-2,6.5-2h0A5,5,0,0,0,11.681,9" fill="none" stroke="#aaaaaa" stroke-linecap="round" stroke-linejoin="round" data-cap="butt" data-color="color-2"></path> <path d="M11.5,4c0,1.933-1.567,4.5-3.5,4.5S4.5,5.933,4.5,4a3.5,3.5,0,0,1,7,0Z" fill="none" stroke="#aaaaaa" stroke-linecap="round" stroke-linejoin="round" data-cap="butt"></path></g></svg></div> <div v-bind:title="`User ID: NONE`">Super Admin</div>
+          </div>
+          <div class="_app__view">
+          <h3 class="_heading">Users</h3>
+          <table class="_table">
+            <tr v-for="user in users" :key="user.id">
+              <td>
+                <span v-if="editableUser !== user.id">{{user.firstName}} {{user.lastName}} <span class="_user__id">{{user.id}}</span></span>
+                <span v-if="editableUser === user.id">
+                  <input type="text" name="firstName" v-model.trim="user.firstName" placeholder="First name"> <input type="text" name="lastName" v-model.trim="user.lastName" placeholder="Last name">
+                </span>
+              </td>
+              <td>
+                <span v-if="user.isAdmin && editableUser !== user.id">Admin</span> <label v-if="editableUser === user.id"><input type="checkbox" name="isAdmin" v-model.trim="user.isAdmin"> Admin</label>
+              </td>
+              <td class="_table__actions">
+                <button class="_btn" v-if="editableUser !== user.id" @click.prevent="editUser(user.id)">Edit</button>
+                <button class="_btn" v-if="editableUser === user.id" @click.prevent="editUser(user.id)">Save</button>
+              </td>
+              <td class="_table__delete">
+                <div v-if="editableUser !== user.id" @click.prevent="destroyUser(user.id)"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" width="16" height="16"><g class="nc-icon-wrapper" fill="#ed6c61"><path fill="#ed6c61" d="M14.7,1.3c-0.4-0.4-1-0.4-1.4,0L8,6.6L2.7,1.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4L6.6,8l-5.3,5.3 c-0.4,0.4-0.4,1,0,1.4C1.5,14.9,1.7,15,2,15s0.5-0.1,0.7-0.3L8,9.4l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L9.4,8l5.3-5.3C15.1,2.3,15.1,1.7,14.7,1.3z"></path></g></svg></div>
+              </td>
+            </tr>
+          </table>
+          <h3 class="_heading">Create New User</h3>
+          <form class="_form">
+            <input type="text" name="firstName" v-model.trim="newUser.firstName" placeholder="First name">
+            <input type="text" name="lastName" v-model.trim="newUser.lastName" placeholder="Last name">
+            <label><input type="checkbox" name="isAdmin" v-model.trim="newUser.isAdmin"> Admin User</label><br>
+            <input type="submit" class="_btn _btn--primary" @click.prevent="createUser()" value="Create">
+          </form>
 
-    <h1>Users</h1>
-    <table>
-      <tr v-for="user in users" :key="user.id">
-        <td>
-          {{user.id}}
-        </td>
-        <td>
-          <span v-if="editableUser !== user.id">{{user.firstName}} {{user.lastName}}</span><br>
-          <span v-if="editableUser === user.id">
-            <input type="text" name="firstName" v-model.trim="user.firstName"> <input type="text" name="lastName" v-model.trim="user.lastName">
-          </span>
-        </td>
-        <td>
-          <span v-if="user.isAdmin && editableUser !== user.id">isAdmin</span><br>
-          <label v-if="editableUser === user.id"><input type="checkbox" name="isAdmin" v-model.trim="user.isAdmin"> isAdmin</label>
-        </td>
-        <td>
-          <button v-if="editableUser !== user.id" @click.prevent="editUser(user.id)">Edit</button>
-          <button v-if="editableUser === user.id" @click.prevent="editUser(user.id)">Save</button>
-          <button v-if="editableUser !== user.id" @click.prevent="destroyUser(user.id)">Delete</button>
-        </td>
-      </tr>
-    </table>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- <h1>Create Thing</h1>
     <form>
